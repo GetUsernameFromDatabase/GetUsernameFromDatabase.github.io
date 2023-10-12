9052cf61a79d5d7145ae57ed61e3a3da58b42580
Ryan Kruberg <rkruberg@gmail.com>
2023-10-13 00:25:45 +0300

    fix: active route not active in navlink

--- src/components/main-menu.tsx
+++ src/components/main-menu.tsx
@@ -10,13 +10,17 @@ const menuItems = [
 
 const MainMenu = () => {
   return (
-    <div className="text-right -mt-10 hidden lg:block">
-      <ul className="md:inline-flex gap-4 py-6 px-10 rounded-3xl bg-slate-50 dark:bg-black menu">
+    <div className='text-right -mt-10 hidden lg:block'>
+      <ul className='md:inline-flex gap-4 py-6 px-10 rounded-3xl bg-slate-50 dark:bg-black menu'>
         {menuItems.map((menuItem, index) => (
           <li key={index}>
             <NavLink
               to={menuItem.to}
-              className="text-slate-900 w-20 hover:text-slate-50 text-1xl py-4 rounded-xl flex items-center flex-col justify-center bg-light-gray dark:bg-mid-dark dark:text-slate-100 hover:hover_active"
+              className={({ isActive }) =>
+                `text-slate-900 w-20 hover:text-slate-50 text-1xl py-4 rounded-xl flex items-center flex-col justify-center bg-light-gray dark:bg-mid-dark dark:text-slate-100 hover:hover_active ${
+                  isActive ? 'active' : ''
+                }`
+              }
             >
               <i className={menuItem.icon}></i>
               {menuItem.text}
