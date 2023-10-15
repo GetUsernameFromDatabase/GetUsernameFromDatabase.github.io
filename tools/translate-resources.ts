7708f4c932b6919391aee60de4bcef159e1a755e
Ryan Kruberg <rkruberg@gmail.com>
2023-10-15 23:15:13 +0300

    feat: translate arrays

--- tools/translate-resources.ts
+++ tools/translate-resources.ts
@@ -1,6 +1,6 @@
 /* eslint-disable security/detect-object-injection */
 /* eslint-disable security/detect-non-literal-fs-filename */
-import fs from 'node:fs'; // Using fs.promises for asynchronous file operations
+import fs from 'node:fs';
 import path from 'node:path';
 
 import translate from 'translate';
@@ -9,10 +9,11 @@ interface LocaleFile {
   path: string;
   lang: string;
 }
-
-interface TranslationFile {
-  [key: string]: string | TranslationFile;
-}
+type TranslationSource =
+  | string
+  | string[]
+  | { [key: string | number]: TranslationSource }
+  | TranslationSource[];
 
 const suppliedArguments: string[] = process.argv.slice(2);
 
@@ -64,47 +65,42 @@ async function getJsonFilesFromDirectory(
   return files;
 }
 
-async function translateFile(file: TranslationFile, locale: LocaleFile) {
-  const translatedResources: TranslationFile = {};
+async function translateObject(
+  initialSource: TranslationSource,
+  locale: LocaleFile,
+) {
+  // TODO: make it account for translation functions https://www.i18next.com/translation-function/essentials
 
-  const eachCurrentLevel = async (
-    object: TranslationFile,
-    parentKey: string,
-  ) => {
-    for (const key in object) {
-      const value = object[key];
-      const nextParent = parentKey ? `${parentKey}.${key}` : key;
-      if (typeof value === 'string') {
-        const translationOptions = {
-          to: locale.lang || 'en',
-        };
-        translatedResources[`${nextParent}`] = await translate(
-          value,
-          translationOptions,
-        );
-      } else if (typeof value === 'object') {
-        await eachCurrentLevel(value, `${nextParent}`);
+  async function recursiveTranslator(
+    source: TranslationSource,
+  ): Promise<TranslationSource> {
+    const currentLevel: TranslationSource = {};
+
+    switch (typeof source) {
+      case 'string': {
+        // !!! DO NOT STORE TRANSLATE OPTIONS AS A SEPERATE VARIABLE -- caching issues
+        const translated = await translate(source, { to: locale.lang ?? 'en' });
+        return translated;
+      }
+      case 'object': {
+        if (Array.isArray(source)) {
+          return Promise.all(
+            source.map((element) => recursiveTranslator(element)),
+          );
+        }
+
+        for (const key in source) {
+          currentLevel[key] = await recursiveTranslator(source[key]);
+        }
+        return currentLevel;
+      }
+      default: {
+        console.error(`Will not process type ${typeof source}:`, source);
       }
     }
-  };
-  await eachCurrentLevel(file, '');
-
-  const parseObjectLevel: TranslationFile = {};
-  for (const key in translatedResources) {
-    const value = translatedResources[key];
-    const keySplit = key.split('.');
-    const keyLevel = keySplit.length;
-
-    let currentParent = parseObjectLevel;
-    for (let index = 0; index < keyLevel; index++) {
-      const currentKey = keySplit[index];
-      if (!currentParent[currentKey]) currentParent[currentKey] = {};
-      if (index === keyLevel - 1) currentParent[currentKey] = value;
-      currentParent = currentParent[currentKey] as TranslationFile;
-    }
+    return currentLevel;
   }
-
-  return parseObjectLevel;
+  return await recursiveTranslator(initialSource);
 }
 
 async function getFilesToTranslate(
@@ -134,16 +130,17 @@ async function main() {
   );
 
   const fileContent = fs.readFileSync(sourceLocalePath, 'utf8');
-  const file: TranslationFile = JSON.parse(fileContent);
+  const file: TranslationSource = JSON.parse(fileContent);
 
   // @ts-expect-error engine does exist in translate -- https://www.npmjs.com/package/translate#engines
   console.log(`Starting translation with "${translate.engine}" engine...`);
 
   for (const locale of listLocaleToTranslate) {
-    console.log(`Translating ${locale.lang}... ${locale.path}`);
+    console.log(`Translating ${locale.lang} | ${locale.path}`);
     try {
-      const t = await translateFile(file, locale);
-      fs.writeFileSync(locale.path, JSON.stringify(t, null, 2));
+      const translatedObject = await translateObject(file, locale);
+      const jsonString = JSON.stringify(translatedObject, null, 2);
+      fs.writeFileSync(locale.path, jsonString);
     } catch (error) {
       console.error(error);
     }
