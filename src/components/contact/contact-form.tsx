7d2d22744976b35a5a27c6755cdac6cd5e244ae6
Ryan Kruberg <rkruberg@gmail.com>
2023-10-22 17:04:43 +0300

    change: use shad components for email form

--- src/components/contact/contact-form.tsx
+++ src/components/contact/contact-form.tsx
@@ -1,5 +1,12 @@
 import { useTranslation } from 'react-i18next';
 
+import { Button } from '../ui/button';
+import { Input } from '../ui/input';
+import { Label } from '../ui/label';
+import { Textarea } from '../ui/textarea';
+
+import { capitalizeWords } from '@/utils/string-manipulation';
+
 interface ContactViaMailFormProperties {
   recipientEmail: string;
 }
@@ -14,40 +21,24 @@ const ContactViaMailForm: React.FC<ContactViaMailFormProperties> = ({
       action={`mailto:${properties.recipientEmail}`}
     >
       <div className="mb-4">
-        <label
-          htmlFor="subject"
-          className="block text-sm font-semibold text-primary"
-        >
-          {t('forms.subject', { ns: 'common' })}:
-        </label>
-        <input
-          type="text"
-          name="subject"
-          required
-          className="w-full rounded-md border border-primary/20 bg-input p-2 text-primary focus:border-blue-500 focus:outline-none"
-        />
+        <Label htmlFor="subject" className="font-semibold">
+          {capitalizeWords(t('forms.subject', { ns: 'common' }))}
+        </Label>
+        <Input type="text" name="subject" required />
       </div>
       <div className="mb-4">
-        <label
-          htmlFor="body"
-          className="block text-sm font-semibold text-primary"
-        >
-          {t('forms.body', { ns: 'common' })}:
-        </label>
-        <textarea
-          id="body"
-          name="body"
-          required
-          className="h-32 w-full rounded-md border border-primary/20 bg-input p-2 text-primary focus:border-blue-500 focus:outline-none"
-        />
+        <Label htmlFor="body" className="font-semibold">
+          {capitalizeWords(t('forms.body', { ns: 'common' }))}
+        </Label>
+        <Textarea id="body" name="body" required />
       </div>
-      <button
+      <Button
         type="submit"
         title={t('forms.send-email-tooltip', { ns: 'common' })}
-        className=" w-full rounded-md bg-primary py-2 font-semibold text-secondary hover:bg-primary/80 hover:shadow-md focus:outline-none"
+        className="w-full"
       >
         {t('forms.send-email', { ns: 'common' })}
-      </button>
+      </Button>
     </form>
   );
 };
