b85212091a90379448df5c332cd1de58c3bb473d
Ryan Kruberg <rkruberg@gmail.com>
2023-10-23 23:49:18 +0300

    feat: int range type utility

--- @types/my-type-utilities.d.ts
+++ @types/my-type-utilities.d.ts
@@ -7,3 +7,15 @@ export type FlattenKeys<T, P = ''> = {
     ? `${P}${K}`
     : never;
 }[keyof T];
+
+export type Enumerate<
+  N extends number,
+  Accumulator extends number[] = [],
+> = Accumulator['length'] extends N
+  ? Accumulator[number]
+  : Enumerate<N, [...Accumulator, Accumulator['length']]>;
+
+export type IntRange<F extends number, T extends number> = Exclude<
+  Enumerate<T>,
+  Enumerate<F>
+>;
