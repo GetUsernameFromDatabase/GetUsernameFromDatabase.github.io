00d688231e91355eca08114d3aa274746122eb8a
Ryan Kruberg <rkruberg@gmail.com>
2023-10-23 23:55:39 +0300

    feat: truncate text

--- src/utils/string-manipulation.ts
+++ src/utils/string-manipulation.ts
@@ -4,3 +4,19 @@ export function capitalizeWords(input: string): string {
     .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
     .join(' ');
 }
+
+/**
+ * Was supposed to be for truncating text -- decided to use CSS instead
+ */
+export function truncateText(input: string, maxLength = 50): string {
+  const tooLongString = '...';
+  /** Max length that takes {@link tooLongString} into account */
+  const trueMaxLength = maxLength - tooLongString.length;
+
+  // I don't want to cut up words
+  const lastSpaceIndex = input.lastIndexOf(' ', trueMaxLength);
+  if (lastSpaceIndex === -1) {
+    return input.slice(0, trueMaxLength) + tooLongString;
+  }
+  return input.slice(0, lastSpaceIndex) + tooLongString;
+}
