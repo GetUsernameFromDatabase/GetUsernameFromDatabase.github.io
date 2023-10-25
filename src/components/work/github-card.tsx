8bff85fc0de21a95f3ac8a41ace83c94bd3bdb6d
Ryan Kruberg <rkruberg@gmail.com>
2023-10-25 03:28:15 +0300

    dev: wip work page

---
+++ src/components/work/github-card.tsx
@@ -0,0 +1,76 @@
+import { CircleIcon, LinkIcon, StarIcon } from 'lucide-react';
+import type { DateTime } from 'luxon';
+import { useTranslation } from 'react-i18next';
+import { Link } from 'react-router-dom';
+
+import { Button } from '../ui/button';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '../ui/card';
+
+import { cn } from '@/lib/utils';
+
+interface GithubCardProperties {
+  title: string;
+  description: string | React.ReactNode;
+  link: string;
+  updated_at?: DateTime | null;
+  starCount?: number;
+  mainLanguage?: string | null;
+}
+
+const getLanguageStyle = (language?: string) => {
+  console.log(language);
+  const defaultClassName = 'fill-sky-400 text-sky-400';
+  return defaultClassName;
+};
+
+function GithubCard(properties: GithubCardProperties) {
+  const { t } = useTranslation();
+  return (
+    <Card>
+      <CardHeader className="grid grid-cols-[1fr_110px] items-start gap-4 space-y-0">
+        <div className="space-y-1">
+          <CardTitle>{properties.title}</CardTitle>
+          <CardDescription>{properties.description}</CardDescription>
+        </div>
+        <Button variant="secondary" size="icon" className="ml-auto" asChild>
+          <Link to={properties.link} target="_blank">
+            <LinkIcon></LinkIcon>
+          </Link>
+        </Button>
+      </CardHeader>
+      <CardContent>
+        <div className="flex space-x-4 text-sm text-muted-foreground">
+          {properties.mainLanguage && (
+            <div className="flex items-center">
+              <CircleIcon
+                className={cn(
+                  'mr-1 h-3 w-3',
+                  getLanguageStyle(properties.mainLanguage),
+                )}
+              />
+              {properties.mainLanguage}
+            </div>
+          )}
+
+          <div className="flex items-center">
+            <StarIcon className="mr-1 h-3 w-3" />
+            {properties.starCount}
+          </div>
+          {properties.updated_at && (
+            <div>{`${t('last-updated-at', {
+              ns: 'common',
+            })} ${properties.updated_at.toLocaleString()}`}</div>
+          )}
+        </div>
+      </CardContent>
+    </Card>
+  );
+}
+
+export default GithubCard;
--- src/pages/work-page.tsx
+++ src/pages/work-page.tsx
@@ -1,15 +1,64 @@
+import { DateTime } from 'luxon';
+import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 
 import PageWrapper from '@/components/page/page-wrapper';
+import { DataTable } from '@/components/ui/data-table';
+import { GitDataTableToolbar } from '@/components/work/git-data-table-toolbar';
+import GithubCard from '@/components/work/github-card';
+// import { useOctokit } from '@/hooks/use-octokit';
+import {
+  type GitHubResponseData,
+  gitTableColumns,
+} from '@/info/git-table-columns';
+import storedGitHubRepoState from '@/info/github-repos.json';
+// import { personalInfo } from '@/info/personal';
 import { capitalizeWords } from '@/utils/string-manipulation';
 
 const Work = () => {
   const { t } = useTranslation();
+  // const octokit = useOctokit();
 
+  const [repoData] = useState<GitHubResponseData>(storedGitHubRepoState);
+
+  // TODO: also add gitlab https://gitlab.com/api/v4/users/GetUsernameFromDatabase/projects
+  // TODO: unify github and gitlab, add filtering/sorting
+
+  // TODO: make sure this gets requested as rarely as possible -- possibly use a store (find pinia, https://redux.js.org/ ???)
+  // useEffect(() => {
+  //   async function fetchMyRepos() {
+  //     const request = await octokit.request('GET /users/{username}/repos', {
+  //       username: personalInfo.names.github,
+  //     });
+  //     console.log(request.data);
+  //     setRepoData(request.data);
+  //   }
+  //   fetchMyRepos();
+  // });
   return (
     <PageWrapper title={capitalizeWords(t('work-page.title'))}>
-      TODO: fetch data and display from
-      https://api.github.com/users/GetUsernameFromDatabase/repos
+      <DataTable
+        columns={gitTableColumns}
+        data={repoData}
+        toolbar={(table) =>
+          GitDataTableToolbar({ table, filterColumn: 'name' })
+        }
+      ></DataTable>
+      <div className="space-y-4">
+        {repoData?.map((value, index) => (
+          <GithubCard
+            key={index}
+            title={value.name}
+            link={value.html_url}
+            description={value.description}
+            mainLanguage={value.language}
+            starCount={value.stargazers_count}
+            updated_at={
+              value.updated_at ? DateTime.fromISO(value.updated_at) : null
+            }
+          ></GithubCard>
+        ))}
+      </div>
     </PageWrapper>
   );
 };
